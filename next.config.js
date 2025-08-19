cfbc3fb6e48d8415667c4d74ac006e9fc8e851f2
gates-mini-game <eren47ey@gmail.com>
2025-08-20 01:43:00 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
+const path = require('path');
+
 module.exports = {
   reactStrictMode: true,
+   webpack: (config) => {
+    config.resolve.modules.push(path.join(__dirname, 'public'));
+    return config;
+  },
 };
